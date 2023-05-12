3e6c10346452a9abdfe19862bd5b65cff1ede411
alex <nosoval3ksey@yandex.ru>
2023-05-12 22:01:51 +0300

    що

--- something/te.py
+++ something/te.py
@@ -17,77 +17,74 @@ from keras.models import Sequential
 from keras.layers import MaxPooling1D, BatchNormalization
 from tensorflow.keras.layers import Dense, Flatten, Dropout, Conv2D, MaxPooling2D, Conv1D
 
-# model = Sequential([
-#     Conv1D(32, 30, activation='linear', padding='same', input_shape=(650, 30)),
-#     BatchNormalization(),
-#     MaxPooling1D(pool_size=2),
-#     Conv1D(64, 30, activation='linear', padding='same'),
-#     BatchNormalization(),
-#     MaxPooling1D(pool_size=2),
-#     Conv1D(64, 30, activation='linear'),
-#     # BatchNormalization(),
-#     MaxPooling1D(pool_size=2),
-#     Flatten(),
-#     Dense(128, activation='linear'),
-#     # BatchNormalization(),
-#     Dense(3, activation='softmax'),
-# ])
+stream_info = pylsl.resolve_stream()
+inlet = StreamInlet(stream_info[0], max_chunklen=750)
+
 with open('data.bin', mode='rb') as file:
     chunk_arr = pickle.load(file)
 with open('data2.bin', mode='rb') as f:
     chunk_res = pickle.load(f)
+
 chunk_res = keras.utils.to_categorical(chunk_res, 3)
 chunk_arr = np.expand_dims(chunk_arr, axis=0)
 
 b, a = sgn.butter(3, (8, 13), btype='bandpass', fs=250)
 
 print(chunk_arr.shape)
-
-zi = numpy.zeros((300,650, 30, max(len(a), len(b)) - 1))
+# plt.plot(chunk_arr[0][102])
+# plt.title('before')
+# plt.show()
+zi = numpy.zeros((1,300,650, max(len(a), len(b)) - 1))
 
 
 # z, _ = sgn.lfilter(b, a, chunk_arr, zi=zi)
 # z2, _ = sgn.lfilter(b, a, z, zi=zi)
 # y = sgn.filtfilt(b,a,chunk_arr)
 
-chunk_arr = chunk_arr.reshape(300,650,30,1)
+# chunk_arr = chunk_arr.reshape(30,650,300,1)
 chunk_arr, z = sgn.lfilter(b, a, chunk_arr, zi=zi)
+# plt.plot(chunk_arr[0][102])
+# plt.title('after')
+# plt.show()
+chunk_arr = chunk_arr.reshape(300,30,650,1)
 print(chunk_arr.shape)
 # X_train      = X_train.reshape(X_train.shape[0], chans, samples, kernels)
-model = EEGNet(nb_classes = 3, Chans = 650, Samples = 30,
-               dropoutRate = 0.5, kernLength = 8)
+model = EEGNet(nb_classes = 3, Chans = 30, Samples = 650,
+               dropoutRate = 0.5,  kernLength = 32, F1 = 8, D = 2, F2 = 16,
+               dropoutType = 'Dropout')
 print(model.summary())
-# print(chunk_arr[0].shape)
-# print(chunk_arr.shape)
+
+
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(len(chunk_arr))
 print(len(chunk_res))
-history = model.fit(chunk_arr, chunk_res, epochs=5, validation_split=0.2)
+history = model.fit(chunk_arr, chunk_res, epochs=45, validation_split=0.2)
 
-stream_info = pylsl.resolve_stream()
-inlet = StreamInlet(stream_info[0], max_chunklen=750)
 
-# plt.plot(history.history['accuracy'])
-# plt.plot(history.history['val_accuracy'])
-# plt.title('model accuracy')
-# plt.ylabel('accuracy')
-# plt.xlabel('epoch')
-# plt.legend(['accuracy', 'val_accuracy'], loc='upper left')
-# plt.show()
-# plt.plot(history.history['loss'])
-# plt.plot(history.history['val_loss'])
-# plt.title('model loss')
-# plt.ylabel('loss')
-# plt.xlabel('epoch')
-# plt.legend(['loss', 'val_loss'], loc='upper left')
-# plt.show()
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['accuracy', 'val_accuracy'], loc='upper left')
+plt.show()
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['loss', 'val_loss'], loc='upper left')
+plt.show()
 while True:
     chunk, t_ = inlet.pull_chunk(timeout=3)  # Chunk [750x31(?)]
     chunk = chunk[:650]
     x = np.expand_dims(chunk, axis=0)
-    print(x.shape)
-    x = x.reshape(1,650,30, 1)
-    print(x.shape)
+    b, a = sgn.butter(3, (8, 13), btype='bandpass', fs=250)
+    zi = numpy.zeros(( 1, 650, max(len(a), len(b)) - 1))
+    x, z = sgn.lfilter(b, a, x, zi=zi)
+    # print(x.shape)
+    x = x.reshape(1,30,650, 1)
+    # print(x.shape)
     res = model.predict(x)
     if np.where(res[0] == max(res[0]))[0] == 0:
         print("Left")
